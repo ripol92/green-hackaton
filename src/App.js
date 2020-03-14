00cd2ed904ffc229f2001df168f3c61d91cca514
Furkat Subkhonkulov <subhonkulov2@gmail.com>
2020-03-14 16:31:39 +0500

    added map

--- src/App.js
+++ src/App.js
@@ -1,26 +1,41 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Map from "./app/components/Map";
+import { makeStyles } from '@material-ui/core/styles';
+import Grid from '@material-ui/core/Grid';
+import Header from "./app/components/Header";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const useStyles = makeStyles(theme => ({
+    root: {
+        flexGrow: 1,
+    },
+    paper: {
+        padding: theme.spacing(2),
+        textAlign: 'center',
+        color: theme.palette.text.secondary,
+    },
+}));
+
+
+class App extends React.Component{
+    constructor(props) {
+        super();
+    }
+    render() {
+        return (
+            <div>
+                <Header/>
+                <Grid container>
+                    <Grid item xs={4} md={4} xl={4}>
+
+                    </Grid>
+                    <Grid item xs={8} md={8} xl={8}>
+                        <Map/>
+                    </Grid>
+                </Grid>
+            </div>
+        );
+    }
 }
 
 export default App;
---
+++ src/app/components/EventDetailDialog.js
@@ -0,0 +1,40 @@
+import React from "react";
+import {Dialog, DialogActions, DialogContent, DialogTitle} from "@material-ui/core";
+import Button from '@material-ui/core/Button';
+
+// props:
+//   photo uri
+//   show or not show photo
+
+export default class EventDetailDialog extends React.Component<>{
+    constructor(props) {
+        super();
+        this.handleClose = this.handleClose.bind(this);
+    }
+
+    render() {
+        return (
+            <Dialog open={this.props.showDialog}
+                    onClose={this.handleClose}
+                    aria-labelledby="alert-dialog-title"
+                    aria-describedby="alert-dialog-description"
+            >
+            <DialogTitle id={"alert-dialog-title"}>Photo Title</DialogTitle>
+            <DialogContent>
+                {this.props.location && <img src={this.props.photoUrl} alt="photo"/>}
+                <hr/>
+                {this.props.comment}
+            </DialogContent>
+            <DialogActions>
+                <Button onClick={this.handleClose} color="primary">
+                    Закрыть
+                </Button>
+            </DialogActions>
+            </Dialog>
+        )
+    }
+
+    handleClose() {
+        this.props.onCLose();
+    }
+}
