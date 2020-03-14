71861a92f741271993c23ac02b360ad897f3a35e
shahrom <s.qurbonov@team.alif.tj>
2020-03-14 16:30:38 +0500

    event's sidebar

--- src/App.js
+++ src/App.js
@@ -1,24 +1,36 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
+import Events from './app/components/Events/Events';
+
 function App() {
+  const eventItems = [
+    {
+      id: 0,
+      comment: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry`s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.',
+      longitude: '66.656566',
+      latitude: '38.112544',
+      pathToPhoto: 'https://c8.alamy.com/comp/AWTJNP/recycling-bins-and-a-gabage-can-in-hong-kong-AWTJNP.jpg',
+    },
+    {
+      id: 1,
+      comment: 'Lorem Ipsum has been the industry`s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.',
+      longitude: '66.656566',
+      latitude: '38.112544',
+      pathToPhoto: 'https://marketplace.canva.com/MADat-cj3_Q/1/thumbnail_large-1/canva-a-wooden-gabage-bin-full%2C-outdoors.-MADat-cj3_Q.jpg',
+    },
+    {
+      id: 2,
+      comment: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry.',
+      longitude: '66.656566',
+      latitude: '38.112544',
+      pathToPhoto: 'https://previews.123rf.com/images/joey333/joey3331804/joey333180400018/99911689-gabage-in-thailand-river-after-loy-kratong-night-festival.jpg',
+    },
+  ];
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Events eventItems={eventItems} />
     </div>
   );
 }
---
+++ src/app/components/Events/Events.js
@@ -0,0 +1,58 @@
+import React, { Component } from 'react'
+
+import './events.css';
+
+class Events extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      eventID: -1
+    }
+
+    this.toggle = this.toggle.bind(this);
+  }
+
+  toggle(index, event) {
+    this.setState({
+      eventID: index
+    })
+
+    console.log(event)
+  }
+
+  render() {
+    const eventItems = this.props.eventItems && this.props.eventItems.length > 0 ?
+      this.props.eventItems.map((item, index) => {
+        return (
+          <li className="item" key={index}>
+            <a href="#"
+              onClick={() => this.toggle(index, item)}
+              className={`event ${
+                this.state.eventID === index ? "active" : ""
+                }`} >
+              <p className="comment">{item.comment}</p>
+              {
+                this.state.eventID === index ? <img src={item.pathToPhoto} className="event-img" alt="event-img" /> : <></>
+              }
+            </a>
+          </li>
+        );
+      }) : <></>
+
+    return (
+      <div className="Events">
+        <div className="header">
+          <h1 className="logo"><span className="green">Clean</span> City</h1>
+        </div>
+        <div className="events">
+          <ul className="event-items">
+            {eventItems}
+          </ul>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default Events;
