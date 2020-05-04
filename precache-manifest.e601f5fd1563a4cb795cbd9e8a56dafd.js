13a2a371944d57329b498b85a9f802966ae8b392
Furkat Subkhonkulov <subhonkulov2@gmail.com>
2020-05-04 23:07:13 +0500

    Updates

---
+++ precache-manifest.e601f5fd1563a4cb795cbd9e8a56dafd.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cf78a3ade8aff79faa509b9274e74d51",
+    "url": "/green-hackaton/index.html"
+  },
+  {
+    "revision": "756de0a1f590a5de5d76",
+    "url": "/green-hackaton/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "ad6701affe380ddda93d",
+    "url": "/green-hackaton/static/css/main.86f12bfd.chunk.css"
+  },
+  {
+    "revision": "756de0a1f590a5de5d76",
+    "url": "/green-hackaton/static/js/2.425f887f.chunk.js"
+  },
+  {
+    "revision": "29604c251d47f213ce84615017873961",
+    "url": "/green-hackaton/static/js/2.425f887f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ad6701affe380ddda93d",
+    "url": "/green-hackaton/static/js/main.44e01e5e.chunk.js"
+  },
+  {
+    "revision": "86249656d8081b664b78",
+    "url": "/green-hackaton/static/js/runtime-main.eeefd279.js"
+  },
+  {
+    "revision": "88932dadc42e1bba93b21a76de60ef7a",
+    "url": "/green-hackaton/static/media/Montserrat-Bold.88932dad.ttf"
+  },
+  {
+    "revision": "100b38fa184634fc89bd07a84453992c",
+    "url": "/green-hackaton/static/media/Montserrat-Light.100b38fa.ttf"
+  },
+  {
+    "revision": "a98626e1aef6ceba5dfc1ee7112e235a",
+    "url": "/green-hackaton/static/media/Montserrat-Medium.a98626e1.ttf"
+  },
+  {
+    "revision": "9c46095118380d38f12e67c916b427f9",
+    "url": "/green-hackaton/static/media/Montserrat-Regular.9c460951.ttf"
+  }
+]);
